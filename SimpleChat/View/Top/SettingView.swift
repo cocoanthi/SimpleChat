a60d2ffb5c861eebf930bfa4ef19a0baf10b9399
ryo yaguchi <cocoanthi89@gmail.com>
2025-02-11 18:11:37 +0900

    ✨設定画面のログアウトボタンのデザイン変更・ユーザー情報を表示するようにした

--- SimpleChat/View/Top/SettingView.swift
+++ SimpleChat/View/Top/SettingView.swift
@@ -9,12 +9,48 @@ import SwiftUI
 
 struct SettingView: View {
     @ObservedObject var viewModel: AuthViewModel
-
+    private static let COULD_NOT_GET = "取得できませんでした"
+    
     var body: some View {
-        VStack {
-            Button("Log Out") {
+        VStack(spacing: .zero) {
+            Spacer()
+            // ユーザー情報
+            HStack(spacing: .zero) {
+                VStack(spacing: 6) {
+                    userItem(title: "Account : ", content: CommonViewModel.shared.user?.name)
+                    userItem(title: "Email : ", content: CommonViewModel.shared.user?.email)
+                    userItem(title: "ID : ", content: CommonViewModel.shared.user?.uid)
+                }
+                Spacer()
+            }
+            Spacer()
+            // ログアウト
+            Button("ログアウト") {
                 viewModel.signOut()
             }
+            .buttonStyle(.bordered)
+            Spacer()
         }
+    }
+    
+    /// ユーザー情報表示部分
+    /// - Parameters:
+    ///   - title: 項目のタイトル
+    ///   - content: 項目の内容。nilの場合は”取得できませんでした”
+    /// - Returns: ユーザー情報表示画面
+    @ViewBuilder
+    private func userItem(title: String, content: String?) -> some View {
+        HStack(spacing: .zero) {
+            Text(title)
+                .frame(maxWidth: (UIScreen.main.bounds.width/2)/2, alignment: .trailing)
+            Text(content ?? Self.COULD_NOT_GET)
+                .frame(maxWidth: .infinity, alignment: .leading)
+        }
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView(viewModel: .init())
     }
 }
